cb10e8d7ec8ae8cde7f4e839e1e63f1cbdfb5b0c
Dominik Guzei <dominik.guzei@gmail.com>
2015-03-16 17:01:06 +0100

    updates package.js to recent format

--- package.js
+++ package.js
@@ -1,5 +1,8 @@
 Package.describe({
-  summary: 'Generate database collections filled with content from static files.'
+  summary: 'Generate database collections filled with content from static files.',
+  name: 'codeadventure:electrostatic',
+  version: '1.3.3',
+  git: 'https://github.com/CodeAdventure/meteor-electrostatic.git'
 });
 
 Npm.depends({
@@ -7,13 +10,11 @@ Npm.depends({
   "recursive-readdir": "0.0.2"
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
 
-  api.add_files(
-    ['source/electrostatic.js'],
-    ['server']
-  );
+  api.versionsFrom("METEOR@1.0");
 
+  api.add_files(['source/electrostatic.js'], 'server');
   api.export('Electrostatic');
 
 });
